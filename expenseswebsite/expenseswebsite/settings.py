3d219720f1bee24e372232bbad49441612c9750e
Alexsantossp71 <alex_santos_sp@yahoo.com.br>
2026-08-13 21:37:46 -0300

    fix: use SQLite fallback when no DB_HOST configured (free Render plan)

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -87,16 +87,20 @@ WSGI_APPLICATION = 'expenseswebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+if os.environ.get('DB_HOST'):
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
-
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
-}
 
 
 # Password validation
